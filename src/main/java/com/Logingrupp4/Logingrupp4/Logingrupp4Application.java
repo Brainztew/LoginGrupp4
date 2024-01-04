59d3aa4195830b308abe2822e4135bf10fdc3875
dennis <isacsson_89@hotmail.com>
2024-01-04 15:28:34 +0100

    Start av projekt

---
+++ src/main/java/com/Logingrupp4/Logingrupp4/Logingrupp4Application.java
@@ -0,0 +1,13 @@
+package com.Logingrupp4.Logingrupp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Logingrupp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Logingrupp4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Logingrupp4/Logingrupp4/Logingrupp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Logingrupp4.Logingrupp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Logingrupp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
