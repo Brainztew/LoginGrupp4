19e1b7a1ea464775e912046e5c9775c89b9b737b
dennis <isacsson_89@hotmail.com>
2024-01-08 16:57:47 +0100

    Nu syns inte mina ordrar och fixat så sidan är mer lättnavigerad

--- src/main/java/com/Logingrupp4/Logingrupp4/controller/OrderController.java
+++ src/main/java/com/Logingrupp4/Logingrupp4/controller/OrderController.java
@@ -1,5 +1,16 @@
 package com.Logingrupp4.Logingrupp4.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/orders")
+
 public class OrderController {
-    
+
+    @GetMapping()
+    public String orders() {
+        return "orders";
+    }
 }
