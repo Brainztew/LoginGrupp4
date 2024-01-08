2d1ee043276f60b374abf4e636c6e4a466ac3324
marcusnil94 <marcusnilsson1994@gmail.com>
2024-01-08 16:10:49 +0100

    login och logout fungerar nu

--- src/main/java/com/Logingrupp4/Logingrupp4/repositories/UserRepository.java
+++ src/main/java/com/Logingrupp4/Logingrupp4/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.Logingrupp4.Logingrupp4.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.Logingrupp4.Logingrupp4.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    
+    Optional<User> findByUsername(String username);
+
 }
