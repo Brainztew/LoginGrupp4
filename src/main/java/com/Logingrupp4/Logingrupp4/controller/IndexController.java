fc6c8a4c86a1fed6cf7d841bb5578f7cccca7cf1
dennis <isacsson_89@hotmail.com>
2024-01-10 07:24:22 +0100

    Lite mer css klasser och styling till folket

--- src/main/java/com/Logingrupp4/Logingrupp4/controller/IndexController.java
+++ src/main/java/com/Logingrupp4/Logingrupp4/controller/IndexController.java
@@ -1,13 +1,8 @@
 package com.Logingrupp4.Logingrupp4.controller;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-
 import com.Logingrupp4.Logingrupp4.model.Product;
 import com.Logingrupp4.Logingrupp4.repositories.ProductRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
